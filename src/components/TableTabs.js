fa064b31c0cd41e42c99e9c5e39f8c43c49929be
Jake Louw <dynogic@gmail.com>
2019-08-23 11:54:05 -0400

    Fixed indenting

--- src/components/TableTabs.js
+++ src/components/TableTabs.js
@@ -5,43 +5,43 @@ import Chip from '@material-ui/core/Chip';
 import PropTypes from 'prop-types';
 
 const defaultTabsStyles = theme => ({
-    chip: {
-        marginRight: theme.spacing.unit / 2,
-    },
+  chip: {
+    marginRight: theme.spacing.unit / 2,
+  },
 });
 
 class TableTabs extends React.Component {
-    static propTypes = {
-        classes: PropTypes.object.isRequired,
-        tabs: PropTypes.array.isRequired,
-        onTabSelected: PropTypes.func.isRequired,
-        getTabSelectedId: PropTypes.func.isRequired,
-    };
+  static propTypes = {
+    classes: PropTypes.object.isRequired,
+    tabs: PropTypes.array.isRequired,
+    onTabSelected: PropTypes.func.isRequired,
+    getTabSelectedId: PropTypes.func.isRequired,
+  };
 
-    handleClick = tabSelected => () => {
-        const { onTabSelected } = this.props;
-        onTabSelected(tabSelected);
-        this.forceUpdate();
-    };
+  handleClick = tabSelected => () => {
+    const { onTabSelected } = this.props;
+    onTabSelected(tabSelected);
+    this.forceUpdate();
+  };
 
-    render() {
-        const { classes, tabs, getTabSelectedId } = this.props;
-        const selectedId = getTabSelectedId();
-        return (
-            <Grid>
-                {tabs.map(tab => (
-                    <Chip
-                        key={tab.id}
-                        color={selectedId === tab.id ? 'primary' : 'default'}
-                        label={tab.name}
-                        clickable={true}
-                        className={classes.chip}
-                        onClick={this.handleClick(tab)}
-                    />
-                ))}
-            </Grid>
-        );
-    }
+  render() {
+    const { classes, tabs, getTabSelectedId } = this.props;
+    const selectedId = getTabSelectedId();
+    return (
+      <Grid>
+        {tabs.map(tab => (
+          <Chip
+            key={tab.id}
+            color={selectedId === tab.id ? 'primary' : 'default'}
+            label={tab.name}
+            clickable={true}
+            className={classes.chip}
+            onClick={this.handleClick(tab)}
+          />
+        ))}
+      </Grid>
+    );
+  }
 }
 
 export default withStyles(defaultTabsStyles, { name: 'MUIDataTableTabs' })(TableTabs);
